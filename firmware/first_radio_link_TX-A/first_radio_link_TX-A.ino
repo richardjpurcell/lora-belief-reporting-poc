1225d96a9bebd46afb43219185acd366b933a8aa
richardjpurcell <richard.purcell@dal.ca>
2026-07-02 19:03:38 -0300

    Set Run 031 Condition C startup offsets

--- firmware/first_radio_link_TX-A/first_radio_link_TX-A.ino
+++ firmware/first_radio_link_TX-A/first_radio_link_TX-A.ino
@@ -28,7 +28,7 @@ const char* NODE_ID = "N01";
 const char* SCHEDULE_FILE = "/schedule.csv";
 
 const unsigned long SLOT_INTERVAL_MS = 1000;
-const unsigned long STARTUP_OFFSET_MS = 500;
+const unsigned long STARTUP_OFFSET_MS = 0;
 
 const uint16_t MAX_SCHEDULE_ROWS = 256;
 
